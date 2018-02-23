3ca4b7d9570e5c517dca5be06734950e91e32606
montygoldy <montygoldy2006@gmail.com>
2018-02-23 00:17:33 -0500

    Installed apollo client for react

--- slack-clone-client/.eslintrc.js
+++ slack-clone-client/.eslintrc.js
@@ -1,3 +1,15 @@
 module.exports = {
     "extends": "airbnb"
+    "plugins": [
+      "react",
+      "jsx-a11y",
+      "import"
+    ],
+    rules: {
+      "react/jsx-filename-extension": 0
+    },
+
+    "globals": {
+      "document": 1
+    },
 };
--- slack-clone-client/src/index.js
+++ slack-clone-client/src/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloProvider } from 'react-apollo';
+import Routes from './routes'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  // By default, this client will send queries to the
+  //  `/graphql` endpoint on the same host
+  link: new HttpLink(),
+  cache: new InMemoryCache()
+});
+
+const App = (
+  <ApolloProvider>
+    <Routes />
+  </ApolloProvider>
+)
+
+ReactDOM.render(App, document.getElementById('root'));
 registerServiceWorker();
