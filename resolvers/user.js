3fc5eae41a837fb768983c51dd8dc7248f9ad67a
montygoldy <montygoldy2006@gmail.com>
2018-02-25 14:01:54 -0500

    Refactored formaterrors and configured schema and resolvers for createteam

--- resolvers/user.js
+++ resolvers/user.js
@@ -1,15 +1,6 @@
-import bcrypt from 'bcrypt';
-import _ from 'lodash';
-
 import { tryLogin } from '../auth';
+import formatErrors from '../FormatErrors';
 
-const formatErrors = (e, models) => {
-  if (e instanceof models.sequelize.ValidationError) {
-    //  _.pick({a: 1, b: 2}, 'a') => {a: 1}
-    return e.errors.map(x => _.pick(x, ['path', 'message']));
-  }
-  return [{ path: 'name', message: 'something went wrong' }];
-};
 
 export default {
   Query: {
