882b008b5b0ecd966a419fce2910265f7f3c20dd
montygoldy <montygoldy2006@gmail.com>
2018-03-01 23:14:37 -0500

    Added semantic ui for the add channel form

--- slack-clone-client/src/components/AddChannelModal.js
+++ slack-clone-client/src/components/AddChannelModal.js
@@ -1,15 +1,20 @@
 import React from 'react'
-import { Header, Modal } from 'semantic-ui-react'
+import { Form, Input, Button, Header, Modal } from 'semantic-ui-react'
 import '../style.css';
+
 const ModalModalExample = ({open, onClose}) => (
   <Modal open={open} onClose={onClose}>
-    <Modal.Header>Select a Photo</Modal.Header>
+    <Modal.Header>Add Channel</Modal.Header>
     <Modal.Content>
-      <Modal.Description>
-        <Header>Default Profile Image</Header>
-        <p>We've found the following gravatar image associated with your e-mail address.</p>
-        <p>Is it okay to use this photo?</p>
-      </Modal.Description>
+      <Form>
+        <Form.Field>
+          <Input fluid placeholder="Channel Name"/>
+        </Form.Field>
+        <Form.Group widths="equal" >
+          <Button fluid>Create Channel</Button>
+          <Button fluid>Cancel</Button>
+        </Form.Group>
+      </Form>
     </Modal.Content>
   </Modal>
 )
