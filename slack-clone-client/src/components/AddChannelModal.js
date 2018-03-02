974fe5690c7c9c0c7335afe125169a8181f9c48b
montygoldy <montygoldy2006@gmail.com>
2018-03-01 23:31:30 -0500

    Added formik form for create channel

--- slack-clone-client/src/components/AddChannelModal.js
+++ slack-clone-client/src/components/AddChannelModal.js
@@ -1,22 +1,31 @@
 import React from 'react'
-import { Form, Input, Button, Header, Modal } from 'semantic-ui-react'
+import { Form, Input, Button, Modal } from 'semantic-ui-react'
 import '../style.css';
+import { withFormik } from 'formik';
 
-const ModalModalExample = ({open, onClose}) => (
+const AddChannelModal = ({open, onClose, values, handleChange, handleBlur, handleSubmit, isSubmitting,}) => (
   <Modal open={open} onClose={onClose}>
     <Modal.Header>Add Channel</Modal.Header>
     <Modal.Content>
       <Form>
         <Form.Field>
-          <Input fluid placeholder="Channel Name"/>
+          <Input value={values.name} onChange={handleChange} onBlur={handleBlur} name="name" fluid placeholder="Channel Name"/>
         </Form.Field>
         <Form.Group widths="equal" >
-          <Button fluid>Create Channel</Button>
-          <Button fluid>Cancel</Button>
+          <Button fluid disabled={isSubmitting} onClick={handleSubmit} >Create Channel</Button>
+          <Button fluid disabled={isSubmitting} onClick={onClose} >Cancel</Button>
         </Form.Group>
       </Form>
     </Modal.Content>
   </Modal>
 )
 
-export default ModalModalExample
+export default withFormik({
+  mapPropsToValues: props => ({ name: '' }),
+
+  handleSubmit: (values, { props, setSubmitting }) => {
+    console.log(values);
+    console.log("submitting.....");
+    setSubmitting(false);
+  },
+})(AddChannelModal);
