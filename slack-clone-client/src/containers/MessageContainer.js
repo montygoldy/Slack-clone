ff6d99d9046f8369b485c0f2165a188140bfc0fc
montygoldy <montygoldy2006@gmail.com>
2018-03-13 00:08:46 -0400

    Added semantic ui for the design and used sequelize order to do ascending messages

--- slack-clone-client/src/containers/MessageContainer.js
+++ slack-clone-client/src/containers/MessageContainer.js
@@ -2,10 +2,26 @@ import React from 'react';
 import Messages from '../components/Messages';
 import gql from'graphql-tag';
 import { graphql } from 'react-apollo';
+import { Comment } from 'semantic-ui-react'
 
 const MessageContainer = ({ data: { loading, messages } }) => loading ? null : (
   <Messages>
-    {JSON.stringify(messages)}
+    <Comment.Group>
+      {messages.map( message => (
+        <Comment key={`message-${message.id}`}>
+          <Comment.Content>
+            <Comment.Author as='a'>{message.user.username}</Comment.Author>
+            <Comment.Metadata>
+              <div>{message.created_at}</div>
+            </Comment.Metadata>
+            <Comment.Text>{message.text}</Comment.Text>
+            <Comment.Actions>
+              <Comment.Action>Reply</Comment.Action>
+            </Comment.Actions>
+          </Comment.Content>
+        </Comment>
+      ))}
+    </Comment.Group>
   </Messages>
 );
 
