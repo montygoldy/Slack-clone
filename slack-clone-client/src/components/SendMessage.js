23a0b2d07a1131ee781f812fab1a2a46a7b557ce
montygoldy <montygoldy2006@gmail.com>
2018-03-12 23:00:05 -0400

    Added enter key stroke for form submission and copied submission mutation for addchannelmodal

--- slack-clone-client/src/components/SendMessage.js
+++ slack-clone-client/src/components/SendMessage.js
@@ -9,10 +9,59 @@ const InputWrapper =  styled.div`
   margin: 20px;
 `;
 
+const ENTER_KEY = 13;
+
+
+
 const SendMessage = ({ values, handleChange, handleBlur, handleSubmit, isSubmitting, channelName }) => (
   <InputWrapper>
-    <Input name="message" value={values.message} onChange={handleChange} onBlur={handleBlur} fluid placeholder={`Message # ${channelName}`}/>
+    <Input
+      onKeyDown = {(e) => {
+        if(e.keyCode === ENTER_KEY && !isSubmitting){
+          handleSubmit(e);
+        }
+      }}
+      name="message"
+      value={values.message}
+      onChange={handleChange}
+      onBlur={handleBlur}
+      fluid
+      placeholder={`Message # ${channelName}`}/>
   </InputWrapper>
 );
 
-export default withFormik({ })(SendMessage);
+export default withFormik({
+  mapPropsToValues: props => ({ name: '' }),
+
+  handleSubmit: async (values, { props: { onClose, teamId, mutate }, setSubmitting }) => {
+    await mutate({
+      variables: { teamId, name: values.name },
+      optimisticResponse: {
+        createChannel: {
+          __typename: 'Mutation',
+          ok: true,
+          channel: {
+            __typename: 'Channel',
+            id: -1,
+            name: values.name
+          }
+        }
+      },
+      update: (store, { data: { createChannel } }) => {
+        const { ok, channel } = createChannel;
+        console.log(createChannel);
+        if(!ok){
+          return;
+        }
+
+        const data = store.readQuery({ query: allTeamsQuery });
+        const teamIdx = findIndex(data.allTeams, ['id', teamId]);
+        console.log(data);
+        data.allTeams[teamIdx].channels.push(channel);
+        store.writeQuery({ query: allTeamsQuery, data });
+      }
+    })
+    onClose();
+    setSubmitting(false);
+  },
+})(SendMessage);
