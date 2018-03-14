d4afbbedc3472535eb935724cc95cce38da3e990
montygoldy <montygoldy2006@gmail.com>
2018-03-13 23:17:05 -0400

    Working live messaging between teams

--- resolvers/message.js
+++ resolvers/message.js
@@ -8,13 +8,18 @@ const NEW_CHANNEL_MESSAGE = 'NEW_CHANNEL_MESSAGE';
 export default {
   Subscription: {
     newChannelMessage: {
-      subscribe: withFilter(() => pubsub.asyncIterator(NEW_CHANNEL_MESSAGE), (payload, args) => {
-         return payload.channelId === args.channelId;
-      }),
+      subscribe: withFilter(() => pubsub.asyncIterator(NEW_CHANNEL_MESSAGE), (payload, args) =>
+         payload.channelId === args.channelId,
+      ),
     }
   },
   Message: {
-    user: ({ userId }, args, { models }) => models.User.findOne({ where: {id: userId} }),
+    user: ({ user, userId }, args, { models }) => {
+      if(user){
+        return user;
+      }
+      return models.User.findOne({ where: { id: userId } }, { raw: true });
+    }
   },
   Query: {
     messages: requiresAuth.createResolver(async (parent, {channelId}, {models}) =>
@@ -24,7 +29,20 @@ export default {
     createMessage: requiresAuth.createResolver(async (parent, args, {models, user}) => {
       try{
         const message = await models.Message.create({ ...args, userId: user.id });
-        pubsub.publish(NEW_CHANNEL_MESSAGE, { channelId: args.channenlId, newChannelMessage: message.dataValues});
+        const asyncFunc = async () => {
+          const currentUser  = await models.User.findOne({ where: {id: user.id} });
+
+          pubsub.publish(NEW_CHANNEL_MESSAGE, {
+            channelId: args.channelId,
+            newChannelMessage: {
+              ...message.dataValues,
+              user: currentUser.dataValues,
+            }
+          });
+        }
+
+        asyncFunc();
+
         return true;
       } catch(err){
         console.log(err);
--- slack-clone-client/src/containers/MessageContainer.js
+++ slack-clone-client/src/containers/MessageContainer.js
@@ -4,26 +4,61 @@ import gql from'graphql-tag';
 import { graphql } from 'react-apollo';
 import { Comment } from 'semantic-ui-react'
 
-const MessageContainer = ({ data: { loading, messages } }) => loading ? null : (
-  <Messages>
-    <Comment.Group>
-      {messages.map( message => (
-        <Comment key={`message-${message.id}`}>
-          <Comment.Content>
-            <Comment.Author as='a'>{message.user.username}</Comment.Author>
-            <Comment.Metadata>
-              <div>{message.created_at}</div>
-            </Comment.Metadata>
-            <Comment.Text>{message.text}</Comment.Text>
-            <Comment.Actions>
-              <Comment.Action>Reply</Comment.Action>
-            </Comment.Actions>
-          </Comment.Content>
-        </Comment>
-      ))}
-    </Comment.Group>
-  </Messages>
-);
+const newChannelMesageSubscription = gql`
+  subscription($channelId: Int!){
+    newChannelMessage(channelId: $channelId){
+      id
+      text
+      user{
+        username
+      }
+      created_at
+    }
+  }
+`;
+
+class MessageContainer extends React.Component {
+  componentWillMount(){
+    this.props.data.subscribeToMore({
+      document: newChannelMesageSubscription,
+      variables: {
+        channelId: this.props.channelId
+      },
+      updateQuery: (prev, { subscriptionData }) => {
+        if(!subscriptionData){
+          return prev
+        }
+        return {
+          ...prev,
+          messages: [...prev.messages, subscriptionData.data.newChannelMessage]
+        }
+      }
+    });
+  }
+
+  render(){
+    const { data: { loading, messages } } = this.props;
+    return (loading ? null : (
+      <Messages>
+        <Comment.Group>
+          {messages.map( message => (
+            <Comment key={`message-${message.id}`}>
+              <Comment.Content>
+                <Comment.Author as='a'>{message.user.username}</Comment.Author>
+                <Comment.Metadata>
+                  <div>{message.created_at}</div>
+                </Comment.Metadata>
+                <Comment.Text>{message.text}</Comment.Text>
+                <Comment.Actions>
+                  <Comment.Action>Reply</Comment.Action>
+                </Comment.Actions>
+              </Comment.Content>
+            </Comment>
+          ))}
+        </Comment.Group>
+      </Messages>
+    ))};
+  }
 
 const messagesQuery = gql `
   query($channelId: Int!){
